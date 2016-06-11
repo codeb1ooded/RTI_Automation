a082755903ff4021a914fff77d9ba0888f0ae38a
Saloni Bhartia <Saloni Bhartia>
2016-06-11 20:49:40 +0530

    REPLY QUERY PDF

--- gen_q.php
+++ gen_q.php
@@ -1,19 +1,33 @@
 <?php
 	$con= mysqli_connect("localhost","root","","rti");
 	$qno=$_GET['qno'];
-	session_start();
-	$id=$_SESSION['id'];
-	echo $qno;
-	$data1="SELECT * FROM t2 WHERE id=".$id.";";
-	$query=mysqli_query($con,$data1);
-	$data2=mysqli_num_rows($query);
-	$ac=$data2;
-	//$a=$data2;
 	
-	$data3=mysqli_fetch_array($query);
-	echo" As per the RTI sent to the institute the following information is requested from: ".$data3['map'];
+	session_start();
+	//$qno=$_SESSION['cc'];
+	$id=$_SESSION['id'];
+	
 	echo"</br>";
-	echo $data3['ques'];
-	//"AND q_no=".$qno.
+	echo"</br>";
+	
+	
+	echo" For id :".$id;
+	echo"</br>";
+	echo"</br>";
+	
+	echo "Corresponding to query:".$qno;
+	echo"</br>";
+	$data1="SELECT * FROM t2 WHERE id=".$id." AND q_no=".$qno.";";
+	//echo $data1;
+	$query=mysqli_query($con,$data1);
+	$data2=mysqli_fetch_assoc($query);
+	echo"</br>";
+	
+	echo" As per the RTI sent to the institute the following information is requested from: ".$data2['map']." department";
+	echo"</br>";
+	echo $data2['ques'];
+	echo"</br>";
+	echo"</br>";
+	
+	echo"<a href='new_prev.php'><input type=submit value=Exit></a>";
 	
 	?>
