ee5d57a6cfaf0dd396a21dd536642426649bcb24
Saloni Bhartia <Saloni Bhartia>
2016-06-11 20:50:09 +0530

    REPLY QUERY PDF

--- gen_query_pdf.php
+++ gen_query_pdf.php
@@ -4,22 +4,26 @@
 <?php
 //session_start();
 $b= $_SESSION['q'];
+echo "Start of queries ".$b;
 $d= $_SESSION['v'];
-$id= $_SESSION['oid'];
- $_SESSION['oid']=$id;
+echo "End of queries ".$d;
+//$id= $_SESSION['oid'];
 $c=$d;
 $a=$b;
 $c=$c+1;
-echo $b;
-echo $d;
+//echo $b;
+//echo $d;
 while($a<$c)
 {	
 		echo"<li>";
-				echo"<a href='gen_q.php?qno='".$a."'><input type=submit name=query_btn value=query_no_is_".$a."></a>";
+				echo"<a href='gen_q.php?qno=".$a."' target=_blank><input type=submit name=query_btn value=query_no_is_".$a."></a>";
 		echo"</li>";
 		echo"</br></br></br>";
 	$a=$a+1;
 }
+echo"</br></br></br>";
+echo"Your pdf is generated you can exit:";
+echo"<a href='new_prev.php'><input type=submit value=Exit></a>";
 
 	
 ?>
