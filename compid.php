665764ae4e3ac472c7a16d2e2baae053fc9da34b
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-10-01 21:21:59 +0530

    Closed rti done

--- compid.php
+++ compid.php
@@ -10,26 +10,26 @@
 <body>
 <div class='container'>
 <?php
-	include 'config_database.php'; 
+	include 'config_database.php';
 	$id=$_GET['id'];
-	
+
 	echo "<h4><strong>RTI Details for ID ".$id." are-</strong></h4>" ;
 	$query=" SELECT * FROM add_rti where id=".$id;
     $res=mysqli_query($con, $query);
 	$r1=mysqli_fetch_assoc($res);
-	
+
 	$query=" SELECT * FROM info_about_reply where id=".$id;
     $res=mysqli_query($con, $query);
 	$r2=mysqli_fetch_assoc($res);
-	
+
 	$query=" SELECT * FROM section4 where id=".$id;
     $res=mysqli_query($con, $query);
 	$r3=mysqli_fetch_assoc($res);
-	
+
 	$query=" SELECT * FROM public_authority where id=".$id;
     $res=mysqli_query($con, $query);
 	$r4=mysqli_fetch_assoc($res);
-	
+
 	$query=" SELECT * FROM first_appeal where id=".$id;
     $res=mysqli_query($con, $query);
 	$r5=mysqli_fetch_assoc($res);
@@ -37,7 +37,7 @@
 		echo "<tr>
 				<th>Field</th>
 				<th>Value</th>
-			</tr>";  
+			</tr>";
 		echo "<tr>";
 			echo "<td>RTI Id</td>";
 			echo "<td>".$r1['id']."</td>";
@@ -98,7 +98,7 @@
 			echo "<td>Mode of payment(cheque/DD,cash,IPO)</td>";
 			echo "<td>".$r1['pay_mode']."</td>";
 		echo "</tr>";
-		
+
 		if($r2)
 		{
 			echo "<tr>";
@@ -118,7 +118,7 @@
 				echo "<td>".$r2['faa_info']."</td>";
 			echo "</tr>";
 		}
-		
+
 		if($r3)
 		{
 			echo "<tr>";
@@ -138,7 +138,7 @@
 				echo "<td>".$r3['given_info_date']."</td>";
 			echo "</tr>";
 		}
-		
+
 		if($r4)
 		{
 			echo "<tr>";
@@ -154,7 +154,7 @@
 				echo "<td>".$r4['Areceived_date']."</td>";
 			echo "</tr>";
 		}
-		
+
 		if($r5)
 		{
 			echo "<tr>";
@@ -165,17 +165,9 @@
 				echo "<td>Date of transfer of appeal by the receiving officer to FAA</td>";
 				echo "<td>".$r5['transfer_date']."</td>";
 			echo "</tr>";
-			echo "<tr>";
-				echo "<td>Date of receipt of appeal by FAA(mention name of FAA)</td>";
-				echo "<td>".$r5['faa_receipt_date']."</td>";
-			echo "</tr>";
-			echo "<tr>";
-				echo "<td>Date of communicating decision to appelant</td>";
-				echo "<td>".$r5['meet_date']."</td>";
-			echo "</tr>";
 		}
 	echo "</table><br>";
-	
+
 	$query=" SELECT * FROM t2 where id=".$id;
     $res=mysqli_query($con, $query);
 	$r6=mysqli_fetch_assoc($res);
