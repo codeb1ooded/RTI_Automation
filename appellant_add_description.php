5fa61eed335ba0f8c1a3453c362a00aab09a7cda
saloni1996 <salonibhartia1996@gmail.com>
2016-10-01 22:47:55 +0530

    Appellant interface.php

---
+++ appellant_add_description.php
@@ -0,0 +1,41 @@
+<?php
+if(!isset($_SESSION)){
+	session_start();
+}
+	?>
+<html>
+<head>
+	<title>Add Decision</title>
+	<link rel="stylesheet" href="css/background.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<?php
+$id=$_GET['id'];
+
+$_SESSION['i']=$id;
+
+include 'config_database.php';
+
+echo"<center><h3>Corresponding to Appeal for id ". $id." add decision</h3></center>";
+
+$sql="SELECT * FROM add_rti where id=".$id.";";
+$res= mysqli_query($con,$sql);
+$res1=mysqli_fetch_array($res);
+
+echo "</br></br>";
+echo" <center><h3>Name of appellant: ".$res1['name']."</h3></center> ";
+echo "</br>";
+echo"<form action='save_decision.php' method=POST>";
+
+echo"<center><textarea name=decision rows=10 cols=100></textarea><center>";
+
+echo"</br></br><center><input type=submit class=btn value=Save></center>";
+echo"</form>";
+		echo "</br>" ;
+		echo "<a class='btn' href='appellant_interface.php'>Back</a>" ;
+
+?>
+</body>
+</html>
---
+++ appellant_interface.php
@@ -0,0 +1,49 @@
+<?php
+if(!isset($_SESSION)){
+	session_start();
+}
+?>
+<html>
+<head>
+	<title>Appellant</title>
+	<link rel="stylesheet" href="css/background.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<?php
+
+include'config_database.php';
+$sql="SELECT * FROM first_appeal ";
+$res=mysqli_query($con,$sql);
+$b=mysqli_num_rows($res);
+?>
+<table width=100%>
+	<tr>
+		<th>Id</th>
+		<th>Name</th>
+		<th>Transfer Date</th>
+		<th>Options</th>
+
+	</tr>
+
+<?php
+while($b!=0)
+{
+	$f=mysqli_fetch_array($res);
+	$query="SELECT * FROM add_rti WHERE id=".$f['id'].";";
+	$res2=mysqli_query($con,$query);
+	$f2=mysqli_fetch_array($res2);
+
+	echo"<tr>
+		<th>".$f2['id']."</a></th>
+		<th>".$f2['name']."</a></th>
+		<th>".$f['transfer_date']."</a></th>
+		<th><a href='appeal_option.php?id=".$f2['id']."'>View Options</a></th>
+	</tr>";
+
+$b--;
+}
+echo"</table></br></br>";	
+include 'logoff.html';
+?>
